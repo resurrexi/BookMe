c772c3120edd95657ae8e2e0a12da7d37a1fad2f
Liquan Yang <thedevhiro@gmail.com>
2022-01-24 17:22:22 +0800

    wip: add path for event type

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "scheduler"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<slug:event>/", views.time_picker, name="time_picker"),
 ]
--- scheduler/views.py
+++ scheduler/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Event
 
 
 def index(request):
     return render(request, "scheduler/index.html")
+
+def time_picker(request, event):
+    EVENT_CHOICES = [
+        "phone-call-15-min",
+        "phone-call-30-min",
+        "phone-call-45-min",
+        "phone-call-60-min",
+        "google-meet-15-min",
+        "google-meet-30-min",
+        "google-meet-45-min",
+        "google-meet-60-min",
+    ]
+
+    if event not in EVENT_CHOICES:
+        return redirect("scheduler:index")
+
+    if request.htmx:
+        template = "scheduler/partials/time_picker_form.html"
+    else:
+        template = "scheduler/time_picker.html"
+
+    return render(request, template)
